488d9a63db6b87fcb16cfb9d452aaa9982ac9bad
emanifesto <emmanuelbabayemi37@gmail.com>
2025-10-19 19:56:00 -0400

    fixed bug of incorrect getpinfo argument and no definition for printf()

--- ps.c
+++ ps.c
@@ -10,13 +10,13 @@ extern int getpinfo(struct pstat *ps);
 int
 main()
 {
-    struct pstat *ps;
-    getpinfo(ps);
+    struct pstat ps;
+    getpinfo(&ps);
     int i;
     printf(2, "%10s%10s%10s%10s\n", "PID", "Inuse", "Tickets", "Ticks");
     for(i = 0; i < NPROC; i++){
-        if(ps->pid[i] != 0){
-            printf(2, "%10d%10d%10d%10d\n", ps->pid[i], ps->inuse[i], ps->tickets[i], ps->ticks[i]);
+        if(ps.pid[i] != 0){
+            printf(2, "%10d%10d%10d%10d\n", ps.pid[i], ps.inuse[i], ps.tickets[i], ps.ticks[i]);
         }
     }
 }
